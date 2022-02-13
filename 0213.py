108284c635b24a8cb38a2ad909131d8decc46fd3
derasann <90512546+derasann@users.noreply.github.com>
2022-02-13 14:41:53 +0900

    Add files via upload

---
+++ 0213.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+st.title('Happy valentine')
+
+st.write('Display Image')
+
+if st.checkbox('x2+(y−3x2‾‾√)2=1'):
+    img = Image.open('cardioid.JPG')
+    st.image(img, caption='だいすき', use_column_width=True)
+
+if st.checkbox('こんど一緒にいく'):
+    img = Image.open('Maker.JPG')
+    st.image(img, caption='東京は9月3,4日', use_column_width=True)
+
+
